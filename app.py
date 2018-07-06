d99ae238c78df805700d3fdabdc55da0534da5f0
nicholas-miles <nicholas.miles6@gmail.com>
2018-07-06 19:54:24 -0400

    line of best fit

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
+# Dash Libraries
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import pandas as pd
+
+# Plotly Libraries
 from plotly import tools
 import plotly.graph_objs as go
+
+# Scientific libraries
+import pandas as pd
+from numpy import arange,array,ones
+from scipy import stats
+
+# Project Assets
 from dev.db_build import TVShowDatabase
 
 #########################
@@ -46,7 +55,7 @@ def update_graph(imdb_id_list):
         pass
 
 def build_graph(imdb_id):
-    show_name = df_s[df_s.imdb_id == imdb_id]['title'].values[0]
+    show_name = df_s[df_s.imdb_id == imdb_id].title.values[0]
     id_data = df_e[df_e.imdb_id == imdb_id]
     seasons = [int(i) for i in sorted(id_data.season.unique())]
 
@@ -54,23 +63,10 @@ def build_graph(imdb_id):
 
     for s in seasons:
         season_data = id_data[id_data.season == s]
-        hover_text = ["Episode {}: {} ({})".format(e[0],e[1], e[2]) \
-            for e in zip(season_data['ep_num'],season_data['ep_name'], season_data['ep_rating'])]
 
-        trace = go.Scatter(
-            x=season_data['ep_num'],
-            y=season_data['ep_rating'],
-            text=hover_text,
-            hoverinfo='text',
-            mode='markers',
-            opacity=0.75,
-            marker={
-                'size': 5
-            },
-            name="season-{}-graph".format(s)
-        )
+        fig.append_trace(scatter_plot(season_data), 1, s)
+        fig.append_trace(best_fit(season_data), 1, s)
 
-        fig.append_trace(trace, 1, s)
         fig['layout']['xaxis{}'.format(s)].update(showgrid=False,
                                                   showticklabels=False,
                                                   zeroline=False)
@@ -78,6 +74,7 @@ def build_graph(imdb_id):
     fig['layout'].update(
         title=show_name,
         hovermode='closest',
+        showlegend=False,
         yaxis=dict(
             autorange=False,
             range=[0,10],
@@ -86,6 +83,45 @@ def build_graph(imdb_id):
 
     graph = dcc.Graph(id=imdb_id + "-graph", figure=fig)
     return graph
+
+def scatter_plot(season_data):
+    season = season_data.season.values[0]
+
+    hover_text = ["Episode {}: {} ({})".format(e[0],e[1], e[2]) \
+        for e in zip(season_data['ep_num'],season_data['ep_name'], season_data['ep_rating'])]
+
+    scatter = go.Scatter(
+        x=season_data['ep_num'],
+        y=season_data['ep_rating'],
+        text=hover_text,
+        hoverinfo='text',
+        mode='markers',
+        opacity=0.3,
+        marker={
+            'size': 5
+        },
+        name="season-{}-graph".format(season)
+    )
+
+    return scatter
+
+def best_fit(season_data):
+    xi = season_data['ep_num']
+    A = array([xi, ones(9)])
+
+    y = season_data['ep_rating']
+    slope, intercept, r_value, p_value, std_err = stats.linregress(xi,y)
+    line = slope*xi+intercept
+
+    figure = go.Scatter(
+        x=xi,
+        y=line,
+        mode='lines',
+        opacity=0.75
+    )
+
+    return figure
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)                  
